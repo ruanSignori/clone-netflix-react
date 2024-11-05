111543fe8ec4126f8e98fe77dd6a49b8877bd1e6
Ruan Signori <86364910+ruanSignori@users.noreply.github.com>
2024-11-05 16:53:34 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './App.css'
+import './App.css';
 import { Tmdb } from './Tmdb';
 import MovieRow from './components/movie_row';
 import FeaturedMovie from './components/featured_movie';
@@ -9,63 +9,51 @@ export default function App() {
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
   const [blackHeader, setBlackHeader] = useState(false);
-  
+
   useEffect(() => {
-    const loadALl = async () => {
-      //Pegando a lista total
+    const loadAll = async () => {
+      // Pegando a lista total
       let list = await Tmdb.getHomeList();
       setMovieList(list);
 
-      //Pegando o Featured
+      // Pegando o Featured
       let originals = list.filter(i => i.slug === 'originals');
-      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
       let chosen = originals[0].items.results[randomChosen];
       let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
       setFeaturedData(chosenInfo);
-    }
+    };
 
-    loadALl();
+    loadAll();
   }, []);
 
   useEffect(() => {
     const scrollListener = () => {
       window.scrollY > 20 ? setBlackHeader(true) : setBlackHeader(false);
-    }
+    };
 
     window.addEventListener('scroll', scrollListener);
     return () => {
       window.removeEventListener('scroll', scrollListener);
-    }
+    };
   }, []);
 
   return (
     <div className="page">
-      
-      <Header black={blackHeader}/>
+      <Header black={blackHeader} />
 
-      {featuredData && 
-        <FeaturedMovie item={featuredData} />
-      }
+      {featuredData && <FeaturedMovie item={featuredData} />}
 
-      <section className='lists'>
+      <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
 
       <footer>
-        <p>Site desenvolvido no video-aula da B7Web</p>
-        <p>Dados pegos do site <a href='https://www.themoviedb.org/'>Themoviedb.org</a></p>
+        <p>Site desenvolvido na video-aula da B7Web</p>
+        <p>Dados pegos do site <a href="https://www.themoviedb.org/">Themoviedb.org</a></p>
       </footer>
-        <script>
-          alert('Este projeto é uma clonagem de uma interface de streaming, desenvolvido para fins de estudos');
-        </script>
-
-      // {movieList.length <= 0 &&
-      //   <div className='loading'>
-      //     <img src='https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif' alt='Carregando' width={window.innerWidth / 2} height={window.innerHeight / 2} />
-      //   </div>
-      // }
     </div>
-  )
+  );
 }
