242c549fd0b520216557f230c591318ed73299ae
Ruan Signori <86364910+ruanSignori@users.noreply.github.com>
2024-11-05 16:44:50 -0300

    Update index.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,7 +6,7 @@ export default function Header({black}) {
         <header className={black ? 'black' : ''}>
            <div className='header--logo'>
                <a href='/'>
-                   <img src='https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg' alt='logo Netflix' />
+                   <img src='public/logo192.png' alt='logo streaming' />
                </a>
            </div>
             <div className='header--user'>
